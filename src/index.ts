1d78976401def66bbf1241a9d7d77414681265d9
Olga Polyakowa <polyakowa.o2011@yandex.ru>
2023-02-03 22:15:42 +0400

    feat: add spa, footer and header

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import './assets/scss/main.scss';
+import App from './components/app';
 
-document.addEventListener('DOMContentLoaded', () => {
-    // app.init();
-});
+const app = new App();
+app.run();
