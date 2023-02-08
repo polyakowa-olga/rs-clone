bb72e580e3f4b1d40aa0a179e5cce69b284013bf
Olga Polyakowa <polyakowa.o2011@yandex.ru>
2023-02-08 23:41:37 +0400

    feat: add some animation

---
+++ src/components/animation/dices-animation/create-dice.ts
@@ -0,0 +1,36 @@
+import Element from "../../templates/element";
+
+const sideData = {
+  commonClass: "dice-side",
+  indivClasses: ["dice-front", "dice-back", "dice-left", "dice-right", "dice-top", "dice-bottom"]
+}
+
+class Dice extends Element {
+  private id: string
+  constructor(tag: string, classes: string[], id: string) {
+    super(tag, classes);
+    this.id = id
+  }
+
+  createDice() {
+
+    const sidesBox = document.createDocumentFragment();
+    for (let i = 0; i < sideData.indivClasses.length; i++) {
+      const indivClass = sideData.indivClasses[i];
+      const side = document.createElement("div");
+      side.classList.add(`${sideData.commonClass}`, `${indivClass}`);
+      side.id = `${this.id}${indivClass.slice(4)}`;
+      side.textContent = `${indivClass.slice(5)}`;
+      sidesBox.append(side);
+    }
+    return sidesBox;
+  }
+
+  returnHTML() {
+    this.container.id = this.id
+    this.container.append(this.createDice());
+    return this.container;
+  }
+}
+
+export default Dice;
---
+++ src/components/animation/dices-animation/throw-settings.ts
@@ -0,0 +1,36 @@
+const throwSettings = [
+  {
+    x: 0,
+    y: 0,
+    size: 18.75,
+    line: 37.5,
+    sizeStep: 0.25,
+    lineStep: 1.75,
+    frontTranslateZStep: 0.5,
+    time1: -550,
+    time2: -500,
+    rotateXStep1: 20,
+    rotateYStep1: 5,
+    rotateXStep2: 65,
+    translateYStep2: 40,
+    speedStep: 1100
+  },
+  {
+    x: 0,
+    y: 0,
+    size: 18.75,
+    line: 37.5,
+    sizeStep: 0.25,
+    lineStep: 1.75,
+    frontTranslateZStep: 0.5,
+    time1: -550,
+    time2: -500,
+    rotateXStep1: 15,
+    rotateYStep1: 10,
+    rotateXStep2: 65,
+    translateYStep2: 0,
+    speedStep: 1000
+  }
+];
+
+export default throwSettings;
